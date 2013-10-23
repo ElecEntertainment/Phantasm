5e73fbcc83d270b36af8e2048484fef5c177d361
Larry1123 <1123larry@live.com>
2013-10-23 11:40:22 -0400

    Darn Tabs DIE!!

--- desktop/src/main/java/net/larry1123/elecentertainment/phantasm/java/MainDesktop.java
+++ desktop/src/main/java/net/larry1123/elecentertainment/phantasm/java/MainDesktop.java
@@ -2,13 +2,12 @@ package net.larry1123.elecentertainment.phantasm.java;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import net.larry1123.elecentertainment.phantasm.core.Main;
 
 public class MainDesktop {
-	public static void main (String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.useGL20 = true;
-		new LwjglApplication(new Main(), config);
-	}
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.useGL20 = true;
+        new LwjglApplication(new Main(), config);
+    }
 }
--- html/src/main/java/net/larry1123/elecentertainment/phantasm/html/MainHtml.java
+++ html/src/main/java/net/larry1123/elecentertainment/phantasm/html/MainHtml.java
@@ -1,19 +1,18 @@
 package net.larry1123.elecentertainment.phantasm.html;
 
-import net.larry1123.elecentertainment.phantasm.core.Main;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.larry1123.elecentertainment.phantasm.core.Main;
 
 public class MainHtml extends GwtApplication {
-	@Override
-	public ApplicationListener getApplicationListener () {
-		return new Main();
-	}
-	
-	@Override
-	public GwtApplicationConfiguration getConfig () {
-		return new GwtApplicationConfiguration(480, 320);
-	}
+    @Override
+    public ApplicationListener getApplicationListener() {
+        return new Main();
+    }
+
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(480, 320);
+    }
 }
